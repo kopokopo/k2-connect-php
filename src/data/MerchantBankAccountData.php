356ee0317b84490ccaebf2eacc7260aa9970b4eb
Nicollet Njora <nicollet.njora@kopokopo.com>
2020-10-29 12:39:26 +0300

    Add settlement accounts data handlers

---
+++ src/data/MerchantBankAccountData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kopokopo\SDK\Data;
+
+class MerchantBankAccountData
+{
+    public function setData($result)
+    {
+        $result = $result['data'];
+
+        $data['id'] = $result['id'];
+        $data['type'] = $result['type'];
+
+        $data['accountNumber'] = $result['attributes']['account_number'];
+        $data['accountName'] = $result['attributes']['account_name'];
+        $data['bankBranchRef'] = $result['attributes']['bank_branch_ref'];
+        $data['settlementMethod'] = $result['attributes']['settlement_method'];
+
+        $data['status'] = $result['attributes']['status'];
+        $data['accountReference'] = $result['attributes']['account_reference'];
+       
+        return $data;
+    }
+}
---
+++ src/data/MerchantWalletData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kopokopo\SDK\Data;
+
+class MerchantWalletData
+{
+    public function setData($result)
+    {
+        $result = $result['data'];
+
+        $data['id'] = $result['id'];
+        $data['type'] = $result['type'];
+
+        $data['phoneNumber'] = $result['attributes']['phone_number'];
+        $data['network'] = $result['attributes']['network'];
+        $data['firstName'] = $result['attributes']['first_name'];
+        $data['lastName'] = $result['attributes']['last_name'];
+
+        $data['status'] = $result['attributes']['status'];
+        $data['accountReference'] = $result['attributes']['account_reference'];
+       
+        return $data;
+    }
+}
