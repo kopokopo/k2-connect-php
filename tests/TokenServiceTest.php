03b000b5b114610958b4523c2248141bf0052642
Nicollet Njora <nicollet.njora@kopokopo.com>
2021-04-06 15:31:26 +0300

    Update tests

--- tests/TokenServiceTest.php
+++ tests/TokenServiceTest.php
@@ -45,6 +45,69 @@ class TokenServiceTest extends TestCase
 
         // Use $tokenRequestClient to create an instance of the TokenService() class
         $this->tokenRequestClient = new TokenService($tokenRequestClient, $options);
+
+        /*
+        *    revokeToken() setup
+        */
+
+        // Create an instance of MockHandler for returning responses for revokeToken()
+        $revokeMock = new MockHandler([
+            new Response(200, []),
+            new RequestException('Error Communicating with Server', new Request('GET', 'test')),
+        ]);
+
+        // Assign the instance of MockHandler to a HandlerStack
+        $revokeHandler = HandlerStack::create($revokeMock);
+
+        // Create a new instance of client using the revokeToken() handler
+        $tokenRevokeClient = new Client(['handler' => $revokeHandler]);
+
+        // Use the $tokenRevokeClient to create an instance of the TokenService() class
+        $this->tokenRevokeClient = new TokenService($tokenRevokeClient, $options);
+
+        /*
+        *    introspectToken() setup
+        */
+
+        // json response to be returned
+        $introspectBody = file_get_contents(__DIR__.'/Mocks/token-introspect.json');
+
+        // Create an instance of MockHandler for returning responses for introspectToken()
+        $introspectMock = new MockHandler([
+            new Response(200, [], $introspectBody),
+            new RequestException('Error Communicating with Server', new Request('GET', 'test')),
+        ]);
+
+        // Assign the instance of MockHandler to a HandlerStack
+        $introspectHandler = HandlerStack::create($introspectMock);
+
+        // Create a new instance of client using the introspectToken() handler
+        $introspectClient = new Client(['handler' => $introspectHandler]);
+
+        // Use the $introspectClient to create an instance of the TokenService() class
+        $this->introspectClient = new TokenService($introspectClient, $options);
+
+        /*
+        *    infoToken() setup
+        */
+
+        // json response to be returned
+        $infoBody = file_get_contents(__DIR__.'/Mocks/token-info.json');
+
+        // Create an instance of MockHandler for returning responses for infoToken()
+        $infoMock = new MockHandler([
+            new Response(200, [], $infoBody),
+            new RequestException('Error Communicating with Server', new Request('GET', 'test')),
+        ]);
+
+        // Assign the instance of MockHandler to a HandlerStack
+        $infoHandler = HandlerStack::create($infoMock);
+
+        // Create a new instance of client using the infoToken() handler
+        $infoClient = new Client(['handler' => $infoHandler]);
+
+        // Use the $infoClient to create an instance of the TokenService() class
+        $this->infoClient = new TokenService($infoClient, $options);
     }
 
     public function testGetTokenSucceeds()
@@ -52,6 +115,60 @@ class TokenServiceTest extends TestCase
         $this->assertArraySubset(
             ['status' => 'success'],
             $this->tokenRequestClient->getToken()
+        );
+    }
+
+    public function testRevokeTokenSucceeds()
+    {
+        $this->assertArraySubset(
+            ['status' => 'success'],
+            $this->tokenRevokeClient->revokeToken([
+                'accessToken' => 'myRand0mAcc3ssT0k3n',
+            ])
+        );
+    }
+
+    public function testRevokeTokenWithNoAccessTokenFails()
+    {
+        $this->assertArraySubset(
+            ['data' => 'You have to provide the accessToken'],
+            $this->tokenRevokeClient->revokeToken([])
+        );
+    }
+
+    public function testIntrospectTokenSucceeds()
+    {
+        $this->assertArraySubset(
+            ['status' => 'success'],
+            $this->introspectClient->introspectToken([
+                'accessToken' => 'myRand0mAcc3ssT0k3n',
+            ])
+        );
+    }
+
+    public function testIntrospectTokenWithNoAccessTokenFails()
+    {
+        $this->assertArraySubset(
+            ['data' => 'You have to provide the accessToken'],
+            $this->introspectClient->introspectToken([])
+        );
+    }
+
+    public function testInfoTokenSucceeds()
+    {
+        $this->assertArraySubset(
+            ['status' => 'success'],
+            $this->infoClient->infoToken([
+                'accessToken' => 'myRand0mAcc3ssT0k3n',
+            ])
+        );
+    }
+
+    public function testInfoTokenWithNoAccessTokenFails()
+    {
+        $this->assertArraySubset(
+            ['data' => 'You have to provide the accessToken'],
+            $this->infoClient->infoToken([])
         );
     }
 }
