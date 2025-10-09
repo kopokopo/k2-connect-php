2017bf3ed0a9628d17d31fa024c1d3876df0a84f
Duncan Ngugi <tech.curious14@gmail.com>
2025-10-09 10:10:41 +0300

    feat(KK-10936): add sdk source to request headers

--- src/requests/BaseRequest.php
+++ src/requests/BaseRequest.php
@@ -4,24 +4,26 @@ namespace Kopokopo\SDK\Requests;
 
 abstract class BaseRequest
 {
-    protected $data;
+    protected array $data;
 
     public function __construct(array $data)
     {
         $this->data = $data;
     }
 
-    public function getAccessToken()
+    public function getAccessToken(): string
     {
         return $this->getRequestData('accessToken');
     }
 
-    public function getHeaders()
+    public function getHeaders(): array
     {
-        return array('Accept' => 'application/json',
-                    'Content-Type' => 'application/json',
-                    'Authorization' => 'Bearer '.$this->getAccessToken(),
-                );
+        return array(
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer '.$this->getAccessToken(),
+            'User-Agent' => 'Kopokopo-PHP-SDK',
+        );
     }
 
     protected function getRequestData($key)
